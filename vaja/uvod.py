6b45f388ec368e72000c1d2de7616e0bc3b9b01c
marcelpozvek7 <marcel.pozvek7@gmail.com>
2026-09-03 09:46:26 +0000

    mjav

--- vaja/uvod.py
+++ vaja/uvod.py
@@ -1,1 +1,9 @@
-print("Hello word")
+def hello():
+    oddelek = input("Kateri oddelek si? ")
+    if oddelek.lower() == "1.ri":
+        print(f"Hello {oddelek}!!!")
+    else:
+        print(f"Hello {oddelek}")
+
+if __name__ == "__main__": 
+    hello()
